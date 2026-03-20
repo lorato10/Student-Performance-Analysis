0ed29c5bcc04d4d120ec95cd1ff8055a1ceb26db
lorato <165929316+lorato10@users.noreply.github.com>
2026-03-20 13:55:13 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,22 @@
-// firebase-config.js (Compat Version)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC53g6ckzySrcyjWm1zDjTcO0SNgXxtY-Q",
-    authDomain: "student-performance-trac-b2ceb.firebaseapp.com",
-    projectId: "student-performance-trac-b2ceb",
-    storageBucket: "student-performance-trac-b2ceb.firebasestorage.app",
-    messagingSenderId: "402817228138",
-    appId: "1:402817228138:web:f6cfdd24212d89886b9913"
+    apiKey: "AIzaSyDbfvuFiX_9TwkLDNyJfURXWVyN1tCv_rA",
+    authDomain: "student-performance-anal-35624.firebaseapp.com",
+    databaseURL: "https://student-performance-anal-35624-default-rtdb.firebaseio.com",
+    projectId: "student-performance-anal-35624",
+    storageBucket: "student-performance-anal-35624.firebasestorage.app",
+    messagingSenderId: "825455726142",
+    appId: "1:825455726142:web:68e0930e9c43dc57d58b6f",
+    measurementId: "G-CDYYYNVKXV"
 };
 
-// Initialize Firebase using the Compat global object
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
